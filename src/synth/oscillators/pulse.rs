528e385bb990ef7cf8caf205e5065620173ec6ba
Dave White <joltedbot@gmail.com>
2025-07-28 14:20:56 -0700

    Updated the layout of the UI as it was way too cramped

--- src/synth/oscillators/pulse.rs
+++ src/synth/oscillators/pulse.rs
@@ -4,7 +4,7 @@ const PI: f32 = std::f32::consts::PI;
 const DEFAULT_X_COORDINATE: f32 = 0.0;
 const DEFAULT_X_INCREMENT: f32 = 1.0;
 
-const DEFAULT_PULSE_WIDTH_ADJUSTMENT: f32 = 0.0;
+const DEFAULT_PULSE_WIDTH_ADJUSTMENT: f32 = 0.5;
 const OSCILLATOR_MOD_TO_PWM_ADJUSTMENT_FACTOR: f32 = 0.5;
 
 pub struct Pulse {
